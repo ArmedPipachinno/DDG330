f2a7a63be2b2a570f14aa18823a40478335e0f82
ArmedPipachu <89252284+ArmedPipachinno@users.noreply.github.com>
2023-09-25 04:45:15 +0700

    Basic Movement
    Movement
    Ball Handler
    Ball Bounce

---
+++ Bound-Is/Assets/Scripts/BallSpawnHandler.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawnHandler : MonoBehaviour
+{
+    [SerializeField] GameObject BallsGrap; // Drag your object prefab here in the Unity Inspector
+    [SerializeField] private Transform LaunchPoint;   // The point from which the object will be launched
+    [SerializeField] float LaunchForce = 10f; // The force at which the object will be launched
+
+    private GameObject SpawnedObject;
+    private bool HoldingObject = false;
+
+    private void Awake()
+    {
+        SpawnObject();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left mouse click
+        {
+            if (HoldingObject)
+            {
+                LaunchObject();
+            }
+            else
+            {
+                SpawnObject();
+            }
+        }
+    }
+
+    private void SpawnObject()
+    {
+        SpawnedObject = Instantiate(BallsGrap, LaunchPoint.position, Quaternion.identity);// Spawn the objectPrefab in front of the player character at the launchPoint
+        SpawnedObject.transform.parent = transform; // Make the player character the parent
+        HoldingObject = true;
+    }
+
+    private void LaunchObject()
+    {
+        if (SpawnedObject != null)
+        {
+            SpawnedObject.transform.parent = null;// Unparent the object
+            Rigidbody rb = SpawnedObject.GetComponent<Rigidbody>(); // Get the rigidbody of the spawned object and apply force forward
+            if (rb != null)
+            {
+                rb.isKinematic = false;
+                rb.AddForce(transform.forward * LaunchForce, ForceMode.Impulse);
+            }
+            // Reset variables
+            SpawnedObject = null;
+            HoldingObject = false;
+        }
+    }
+}
--- Bound-Is/Assets/Scripts/Movement.cs
+++ Bound-Is/Assets/Scripts/Movement.cs
@@ -1,21 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class MovementBod : MonoBehaviour
 {
-    private Rigidbody RigidPlay;
-    private Animator _Animate;
-    private float _Speed = 0f;
-    private float _Dir = 0f;
-    private float _MaxWalk = 1.0f;
-    private float _MaxSpeed = 2.0f;
-    private float _Accel = .75f;
+    [SerializeField] Rigidbody RigidPlay;
+    [SerializeField] float _WalkSpeed = 1.0f;
+    [SerializeField] float _RunSpeed = 2.0f;
+    //private float _Accel = .75f;
+
+    bool Run =false;
 
     private void Awake()
     {
-        TryGetComponent<Rigidbody>(out RigidPlay);
+        RigidPlay = GetComponent<Rigidbody>(); // Get the Rigidbody component
     }
 
     void Update()
@@ -23,107 +20,21 @@ public class Movement : MonoBehaviour
         bool Run = Input.GetKey(KeyCode.LeftShift);
         float HorizontalInput = Input.GetAxis("Horizontal");
         float VerticalInput = Input.GetAxis("Vertical");
-        float TargetSpeed = Run ? _MaxSpeed : _MaxWalk;
+        float TargetSpeed = Run ? _RunSpeed : _WalkSpeed;
 
-        _Speed = Mathf.MoveTowards(_Speed, TargetSpeed * VerticalInput, _Accel * Time.deltaTime);//_Speed = Mathf.MoveTowards(_Speed, VerticalInput, _Decel * Time.deltaTime);
-        _Dir = Mathf.MoveTowards(_Dir, TargetSpeed * HorizontalInput, _Accel * Time.deltaTime); //_Dir = Mathf.MoveTowards(_Dir, HorizontalInput, _Decel * Time.deltaTime);
+        // Calculate the movement direction
+        Vector3 moveDirection = new Vector3(HorizontalInput, 0f, VerticalInput).normalized;
 
-        //RigidPlay = new Vector3(0,0,0);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Run = true;
+        }
+
+        // Calculate the velocity based on the direction and speed
+        Vector3 velocity = moveDirection * TargetSpeed;
+
+        // Move the object based on the calculated velocity
+        Vector3 newPosition = RigidPlay.transform.position + velocity * Time.deltaTime;
+        RigidPlay.MovePosition(newPosition);
     }
-
 }
-/*
-// Start is called before the first frame update
-void Start()
-{
-    //GetComponent<MouseCursor>();
-}
-
-        /*
-float HorizontalInput = Input.GetAxis("Mouse X");
-float VerticalInput = Input.GetAxis("Mouse Y");
-transform.Rotate(0f, HorizontalInput, 0f, Space.World);
-transform.Rotate(-VerticalInput, 0f, 0f, Space.Self);
-if (Input.GetKey(KeyCode.W))
-{
-    transform.position += Vector3.forward * speed * Time.deltaTime;//new Vector3(0,0, speed * Time.deltaTime);
-}
-else if(Input.GetKey(KeyCode.S))
-{ 
-    transform.position += Vector3.back * speed * Time.deltaTime;//new Vector3(0, 0, speed * Time.deltaTime);
-}
-if (Input.GetKey(KeyCode.A)) 
-{
-    transform.position += Vector3.left * speed * Time.deltaTime;
-}
-else if (Input.GetKey(KeyCode.D)) 
-{
-    transform.position += Vector3.right * speed * Time.deltaTime;
-}
-
-if (Input.GetMouseButton(0))
-{
-    transform.position += Vector3.up * speed * Time.deltaTime;//new Vector3(0, speed * Time.deltaTime, 0);
-}
-else if (Input.GetMouseButton(1)) 
-{
-    transform.position += Vector3.down * speed * Time.deltaTime;
-}
-
-
-//transform.TransformDirection;
-//Vector3.Normalize(transform.position);
-
-reference->watch?v=7kGCrq1cJew
-*/
-
-/*
- *  //[field:SerializeField] float Speed = 0.05f;
-    //[field: SerializeField] float ElevationSpeed = 5.0f;
-    //[field: SerializeField] float RotateSpeed = 0.5f;
-
-    //void Update()
-    //{
-    //    CamRotate();
-
-    //    MoveCamRelate();
-
-    //    MouseElevate();
-
-    //}   
-
-    //void CamRotate()
-    //{
-    //    float HorizontalInput = Input.GetAxis("Mouse X");
-    //    float VerticalInput = Input.GetAxis("Mouse Y");
-    //    transform.Rotate(0f, HorizontalInput * RotateSpeed, 0f, Space.World);
-    //    transform.Rotate(-VerticalInput * RotateSpeed, 0f, 0f, Space.Self);
-    //}
-
-    //void MoveCamRelate()
-    //{
-    //    float PlayerHorizontal = Input.GetAxis("Horizontal");
-    //    float PlayerVertical = Input.GetAxis("Vertical");
-
-    //    Vector3 forward = Camera.main.transform.forward;
-    //    Vector3 Right = Camera.main.transform.right;
-
-    //    Vector3 ForwardRelativeVerticalInput = PlayerVertical * forward * Speed;
-    //    Vector3 RightRelativeHorizontalInput = PlayerHorizontal * Right * Speed;
-
-    //    Vector3 CameraRelativeMovement = ForwardRelativeVerticalInput + RightRelativeHorizontalInput;
-    //    this.transform.Translate(CameraRelativeMovement, Space.World);
-    //}
-
-    //void MouseElevate()
-    //{
-    //    if (Input.GetMouseButton(0))
-    //    {
-    //        transform.position += Vector3.up * ElevationSpeed * Time.deltaTime;//new Vector3(0, speed * Time.deltaTime, 0);
-    //    }
-    //    else if (Input.GetMouseButton(1))
-    //    {
-    //        transform.position += Vector3.down * ElevationSpeed * Time.deltaTime;
-    //    }
-    //}
- */
